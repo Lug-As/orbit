6f0dab8458ca3f12862da4713ef9ab4d56321eed
Artyom Sokol <skaliushartem@gmail.com>
2021-03-05 13:32:53 +0300

    Router links in layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,7 +13,6 @@
 								<img src="../assets/img/Логотип.png" alt=""></picture>
 						</a>
 					</div>
-					<!-- header__img -->
 
 					<div class="header__menu">
 						<ul class="header__menu-ul">
@@ -28,25 +27,20 @@
 									</li>
 								</ul>
 							</li>
-							<!-- /.header__menu-li -->
 							<li class="header__menu-li">
-								<a href="blogers.html" class="header__menu-link">Блогеры</a>
+								<router-link :to="{name: 'AccountsList'}" class="header__menu-link">Блогеры</router-link>
 							</li>
-							<!-- /.header__menu-li -->
 							<li class="header__menu-li">
-								<a href="" class="header__menu-link">Рекламные предложения</a>
+								<router-link :to="{name: 'ProjectsList'}" class="header__menu-link">Рекламные предложения
+								</router-link>
 							</li>
-							<!-- /.header__menu-li -->
 						</ul>
-						<!-- header__menu-ul -->
 					</div>
-					<!-- header__menu -->
 					<div class="header__sign">
 						<div class="header__sign-row" style="display: none;">
 							<button class="header__menu-sign button-grand">Вход</button>
 							<button class="header__menu-sign button-grand">Регистрация</button>
 						</div>
-						<!-- /.header__sign-row -->
 						<div class="header__log-row">
 							<ul class="header__log-ul">
 								<li class="header__menu-li-left">
@@ -62,7 +56,8 @@
 											</h2>
 											<ul class="header__menu-drop width-all">
 												<li class="header__drop-li">
-													<a href="" class="header__li-link">Личный кабинет</a>
+													<router-link :to="{name: 'Profile'}" class="header__li-link">Личный кабинет
+													</router-link>
 												</li>
 												<li class="header__drop-li">
 													<a href="" class="header__li-link">Выход</a>
@@ -72,17 +67,11 @@
 									</ul>
 								</li>
 							</ul>
-							<!-- /.header__log-ul -->
 						</div>
-						<!-- /.header__sign-row -->
 					</div>
-					<!-- /.header__sign -->
 				</div>
-				<!-- header__row -->
 			</div>
-			<!-- container -->
 		</header>
-		<!-- header -->
 		<template>
 			<router-view></router-view>
 		</template>
@@ -94,7 +83,6 @@
 							<source srcset="../assets/img/LogoFooter.webp" type="image/webp">
 							<img src="../assets/img/LogoFooter.png" alt=""></picture>
 					</div>
-					<!-- /.footer__img -->
 					<div class="footer__menu">
 						<ul class="footer__menu-ul">
 							<li class="footer__ul-li">
@@ -113,25 +101,18 @@
 								<a href="" class="footer__li-link">Рекламодателю</a>
 							</li>
 						</ul>
-						<!-- /.footer__text -->
 					</div>
-					<!-- /.footer__menu -->
 					<div class="footer__rights">
 						<div class="footer__rights-contract">
 							<a href="" class="footer__li-link">Договор на обработку персональных данных</a>
 						</div>
-						<!-- /.footer__rights-contract -->
 						<div class="footer__rights-right">
 							<a href="" class="footer__li-link">Орбита 2021 /©Все права защищены!</a>
 						</div>
-						<!-- /.footer__rights-right -->
 					</div>
-					<!-- /.footer__rights -->
 				</div>
-				<!-- container -->
 			</div>
 		</footer>
-		<!-- footer -->
 	</div>
 </template>
 
